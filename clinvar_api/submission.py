1cad11b4680b1440ea2cf0ff3ea774b22c7becbd
Kyle Ferriter <kferrite@broadinstitute.org>
2020-11-18 10:55:13 -0500

    Code upload with runnable main in submission.py. Submit code not implemented

---
+++ clinvar_api/submission.py
@@ -0,0 +1,195 @@
+import os
+import sys
+import argparse
+import csv
+import pandas
+import xlrd
+import datetime
+import json
+
+def generate_excel_colmap(end_after="CS"):
+    """
+    Returns a dict of excel column labels to 0-indexed column indices
+    """
+    alpha = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+    out = [c for c in alpha]
+    stop = False
+    for i in alpha:
+        for j in alpha:
+            label = str(i) + str(j)
+            out.append(label)
+            if label == end_after:
+                stop = True
+                break
+        if stop:
+            break
+    return {out[i]: i for i in range(len(out))}
+
+def parse_citations(s: str):
+    """
+    Parse a citation identifier to a {db, id} map
+    TODO add PubMedCentral, DOI, NCBI Bookshelf
+    Optional.  Citations documenting the clinical significance.
+    Any of PubMed, PubMedCentral, DOI, NCBI Bookshelf combined with the
+    id in that database (e.g. PMID:123456,  PMCID:PMC3385229, NBK:56955).
+    Separate multiple citations by a semicolon.
+    """
+    print("parse_citations(%s)" % s)
+    if pandas.isnull(s) or not s:
+        return []
+    terms = [e.strip() for e in s.split(";")]
+    print("parse_citations: " + str(terms))
+    out = []
+    for term in terms:
+        if term.startswith("PMID"):
+            out.append({"db": "PubMed", "id": term})
+        else:
+            raise RuntimeError("Unknown citation format: " + term)
+    return out
+
+def serialize_date(d: datetime.datetime) -> str:
+    return d.strftime("%Y-%m-%d")
+
+def makedir_if_not_exists(dirname:str):
+    if os.path.exists(dirname):
+        if os.path.isdir(dirname):
+            return
+        else:
+            raise RuntimeError("Path {} already exists".format(dirname))
+    else:
+        os.mkdir(dirname)
+
+# def all_as_string(df: pandas.DataFrame, cols=[]):
+#     for col_name in df.columns:
+#         df[col_name] = df[col_name].astype("string")
+#     return df
+
+def row_to_clinvar_submission(row: pandas.Series, prettyjson=True):
+    row_idx = row.name
+    # misc fields
+    submission_name = "PAHVCEP_10_2020_API"
+    local_id = row["A"]
+    local_key = row["B"]
+    record_status = "novel"
+    release_status = "public"
+    # variantSet
+    variant_set = {
+        "variant": [{
+            "gene": [{"symbol": e.strip() for e in row["C"].split(";")}],
+            "hgvs": row["D"] + ":" + row["E"]
+        }]
+    }
+    # conditionSet
+    condition_set = {
+        "condition": [{
+            "db": row["AD"],
+            "id": row["AE"]
+        }]
+    }
+    # clinicalSignificance
+    clinsig_description = row["AK"]
+    clinsig_date_last_evaluated = serialize_date(row["AL"])
+    clinsig_mode_of_inheritance = row["AO"]
+    clinsig_comment = row["AR"]
+    clinsig_citations = parse_citations(row["AP"])
+    if pandas.notnull(row["AQ"]):
+        clinsig_citations.append({"url": row["AQ"]})
+    # observedIn
+    observed_in = [{
+        "collectionMethod": row["AX"],
+        "alleleOrigin": row["AY"],
+        "affectedStatus": row["AZ"],
+        "numberOfIndividuals": 0
+    }]
+    # assertionCriteria
+    # TODO using hardcoded assertion method and citation for now, update to generalize
+    assertion_criteria = {
+        "citation": {
+            "url": "https://submit.ncbi.nlm.nih.gov/ft/byid/i2ra5ppm/clingen_pah_acmg_specifications_v1.pdf"
+        },
+        "method": "ClinGen PAH ACMG Specifications v1"
+    }
+    doc = {
+        "clinvarSubmission": [{
+                "assertionCriteria": assertion_criteria,
+                "clinicalSignificance": {
+                    "citation": clinsig_citations,
+                    "clinicalSignificanceDescription": clinsig_description,
+                    "comment": clinsig_comment,
+                    "dateLastEvaluated": clinsig_date_last_evaluated,
+                    "modeOfInheritance": clinsig_mode_of_inheritance
+                },
+                "conditionSet": condition_set,
+                "localID": local_id,
+                "localKey": local_key,
+                "observedIn": observed_in,
+                "recordStatus": record_status,
+                "releaseStatus": release_status,
+                "variantSet": variant_set
+            }],
+        "submissionName": submission_name
+    }
+    makedir_if_not_exists("submissions")
+    with open("submissions/submission-%s-%s.json" % (row_idx, local_id), "w") as f_out:
+        indent = None
+        if prettyjson:
+            indent = 4
+        json.dump(doc, f_out, indent=indent)
+    return doc
+
+# input_filename = "ClinVar_Submission_ClinGen_PAHVCEP_10_2020.xlsx"
+# excel_col_labels = [e[0] for e in sorted(generate_excel_colmap().items(), key=lambda e: e[1])]
+# df = pandas.read_excel(input_filename, header=None, names=excel_col_labels, sheet_name="Variant")
+# # df = all_as_string(df)
+# df = df[6:] # ClinVar excel has 6 leading rows 0 through 5 including 5
+# df_json = df.apply(row_to_clinvar_submission, axis=1)
+
+def str_to_bool(s):
+    if s == "false":
+        return False
+    return True
+
+def main(argv):
+    parser = argparse.ArgumentParser("clinvar_api.py")
+    subparsers = parser.add_subparsers(help="subcommand",
+        required=True,
+        dest="subcommand")
+
+    generate_subparser = subparsers.add_parser("generate")
+    generate_subparser.add_argument("--filename", required=True, help="ClinVar submission MS Excel file")
+    generate_subparser.add_argument("--prettyjson", default="true",
+                        choices=["true", "false"],
+                        help="Set to False")
+
+    submit_subparser = subparsers.add_parser("submit")
+    submit_subparser.add_argument("--directory", type=str, help="Submit all files from this directory")
+    submit_subparser.add_argument("--file", type=str, help="Submit this file")
+    submit_subparser.add_argument("--config", required=True, help="Config file for clinvar-api")
+
+    opts = parser.parse_args(argv)
+
+    input_filename = opts.filename
+
+    if opts.subcommand == "generate":
+        opts.prettyjson = str_to_bool(opts.prettyjson)
+        excel_col_labels = [e[0] for e in sorted(generate_excel_colmap().items(), key=lambda e: e[1])]
+        df = pandas.read_excel(input_filename, header=None, names=excel_col_labels, sheet_name="Variant")
+        df = df[6:] # ClinVar excel has 6 leading rows 0 through 5 including 5
+        df_json = df.apply(row_to_clinvar_submission,
+            axis=1,
+            args=(opts.prettyjson,))
+    elif opts.subcommand == "submit":
+        if opts.directory and opts.file:
+            raise RuntimeError("Cannot use --directory and --file at the same time")
+        raise RuntimeError("submit not yet implemented")
+
+
+if __name__ == "__main__":
+    argv = [
+        "generate",
+        "--filename", "ClinVar_Submission_ClinGen_PAHVCEP_10_2020.xlsx",
+        "--prettyjson", "false"
+    ]
+    argv = sys.argv[1:]
+
+    sys.exit(main(argv))
